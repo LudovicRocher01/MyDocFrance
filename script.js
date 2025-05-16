21658bbee098d82c22de6d737d14dfc9b691ae5a
LudovicRocher01 <ludovicrocher01@yahoo.fr>
2025-05-16 18:51:49 +0200

    sans tel

--- script.js
+++ script.js
@@ -19,16 +19,11 @@
             }
         });
     
-// Menu mobile
-document.getElementById('mobile-menu-button').addEventListener('click', function() {
-    var mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.remove('hidden');
-    } else {
-        mobileMenu.classList.add('hidden');
-    }
-});
-
+        // Menu mobile
+        document.getElementById('mobile-menu-button').addEventListener('click', function() {
+            // Implémentez ici le menu mobile si nécessaire
+            alert("Menu mobile à implémenter");
+        });
 
         // Compteurs animés
         function animateCounters() {
